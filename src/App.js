3bcccd84fc09986efa646233897942801323e8b5
Saurabh Kumar <singhsaurabh23@live.com>
2020-09-29 12:34:25 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Saurabh",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Button variant="outline-primary">Primary</Button>{' '} */}
+          <p>{this.state.name}</p>
+          <button
+            type="button"
+            className="btn btn-outline-primary"
+            onClick={() => this.setState({ name: "Singh" })}
+          >
+            Change Name
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React -2
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
